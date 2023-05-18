c9cf5532846d62cb31e252f781a1a0c6266ddb9a
Falron98 <bartekkrystowski@wp.pl>
2023-05-18 15:47:18 +0200

    Init commit

---
+++ app/src/main/java/pjwstk/edu/pl/resistor_calculator/MainActivity.kt
@@ -0,0 +1,11 @@
+package pjwstk.edu.pl.resistor_calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
