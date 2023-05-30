c9cc422066174821258ef68f6f3d0d88bd7742b5
s21162-pj <s21162@pjwstk.edu.pl>
2023-05-30 20:21:13 +0200

    *activity_start.xml:     Added start screen, application logo with its name and shadows under them for a 3d effect. *StartActivity.kt:     'Handler()' replaced with 'Handler(Looper.getMainLooper())'

--- app/src/main/java/pjwstk/edu/pl/resistor_calculator/StartActivity.kt
+++ app/src/main/java/pjwstk/edu/pl/resistor_calculator/StartActivity.kt
@@ -3,6 +3,7 @@ package pjwstk.edu.pl.resistor_calculator
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 
 class StartActivity : AppCompatActivity() {
@@ -14,7 +15,7 @@ class StartActivity : AppCompatActivity() {
         setContentView(R.layout.activity_start)
 
         // Delayed redirect to MainActivity
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
