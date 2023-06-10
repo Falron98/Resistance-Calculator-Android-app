919168c9bf3aeaa05ee28ad16c2b020ad4c959f4
s21162-pj <s21162@pjwstk.edu.pl>
2023-06-11 01:44:48 +0200

    PastResultsActivity.kt - Added function to compare two saved resistors. If there are < 2, then you are redirected to ChooseStripesActivity.
    ChooseStripesActivity.kt
    - Reverted to the previous day's version because the latest commit turned out to be wrong and caused errors with PastResultAcvivity and CalculateResistanceActivity

--- app/src/main/java/pjwstk/edu/pl/resistor_calculator/ChooseStripesActivity.kt
+++ app/src/main/java/pjwstk/edu/pl/resistor_calculator/ChooseStripesActivity.kt
@@ -6,11 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
-import androidx.core.content.ContextCompat
 import java.io.*
 
 class ChooseStripesActivity : AppCompatActivity() {
-
     private lateinit var stripeNumberSpinner: Spinner
     private lateinit var stripeSpinnersContainer: LinearLayout
     private lateinit var submitButton: Button
@@ -19,37 +17,61 @@ class ChooseStripesActivity : AppCompatActivity() {
     private lateinit var deleteButton: Button
     private lateinit var configurationsSpinner: Spinner
     private lateinit var configurationNameEditText: EditText
-
     private val normalColorOptions = arrayOf(
-        "None", "Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Violet", "Gray", "White"
+        "None",
+        "Black",
+        "Brown",
+        "Red",
+        "Orange",
+        "Yellow",
+        "Green",
+        "Blue",
+        "Violet",
+        "Gray",
+        "White"
     )
-
     private val multiplierColorOptions = arrayOf(
-        "None", "Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Violet", "Gray", "White", "Silver", "Gold"
+        "None",
+        "Black",
+        "Brown",
+        "Red",
+        "Orange",
+        "Yellow",
+        "Green",
+        "Blue",
+        "Violet",
+        "Gray",
+        "White",
+        "Silver",
+        "Gold"
     )
-
     private val toleranceColorOptions = arrayOf(
-        "None", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Violet", "Gray", "Silver", "Gold"
+        "None",
+        "Brown",
+        "Red",
+        "Orange",
+        "Yellow",
+        "Green",
+        "Blue",
+        "Violet",
+        "Gray",
+        "Silver",
+        "Gold"
     )
-
     private val temperatureColorOptions = arrayOf(
         "None", "Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Violet", "Gray"
     )
-
-
-    private val savedConfigurations = mutableListOf<Pair<String, List<Int>>>()
+    private val savedConfigurations = mutableListOf<Pair<String, List<String>>>()
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_stripes)
-
         stripeNumberSpinner = findViewById(R.id.stripeNumberSpinner)
         val stripeOptions = arrayOf("3 Stripes", "4 Stripes", "5 Stripes", "6 Stripes")
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, stripeOptions)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         stripeNumberSpinner.adapter = adapter
-
         stripeSpinnersContainer = findViewById(R.id.stripeSpinnersContainer)
         submitButton = findViewById(R.id.submitButton)
         saveButton = findViewById(R.id.saveButton)
@@ -57,7 +79,6 @@ class ChooseStripesActivity : AppCompatActivity() {
         deleteButton = findViewById(R.id.deleteButton)
         configurationsSpinner = findViewById(R.id.configurationsSpinner)
         configurationNameEditText = findViewById(R.id.configurationNameEditText)
-
         stripeNumberSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
                 parent: AdapterView<*>?,
@@ -67,9 +88,7 @@ class ChooseStripesActivity : AppCompatActivity() {
             ) {
                 val selectedStripeOption = stripeOptions[position]
                 val numStripes = selectedStripeOption.split(" ")[0].toInt()
-
                 stripeSpinnersContainer.removeAllViews()
-
                 for (i in 1..numStripes) {
                     val stripeSpinner = Spinner(this@ChooseStripesActivity)
                     val stripeAdapter = ArrayAdapter(
@@ -79,9 +98,7 @@ class ChooseStripesActivity : AppCompatActivity() {
                     )
                     stripeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
                     stripeSpinner.adapter = stripeAdapter
-
                     stripeSpinnersContainer.addView(stripeSpinner)
-
                     stripeSpinner.onItemSelectedListener =
                         object : AdapterView.OnItemSelectedListener {
                             override fun onItemSelected(
@@ -104,7 +121,6 @@ class ChooseStripesActivity : AppCompatActivity() {
                             }
                         }
                 }
-
                 checkSubmitButtonState()
             }
 
@@ -112,36 +128,29 @@ class ChooseStripesActivity : AppCompatActivity() {
                 // Do nothing
             }
         }
-
         submitButton.setOnClickListener {
-            val selectedStripes = mutableListOf<Int>()
-
+            val selectedStripes = mutableListOf<String>()
             for (i in 0 until stripeSpinnersContainer.childCount) {
                 val stripeSpinner = stripeSpinnersContainer.getChildAt(i) as Spinner
-                val selectedColorIndex = stripeSpinner.selectedItemPosition
-                selectedStripes.add(selectedColorIndex)
+                val selectedColor = stripeSpinner.selectedItem.toString()
+                selectedStripes.add(selectedColor)
             }
-
             val intent = Intent(this, CalculateResistanceActivity::class.java)
-            intent.putIntegerArrayListExtra("stripes", ArrayList(selectedStripes))
+            intent.putStringArrayListExtra("stripes", ArrayList(selectedStripes))
             startActivity(intent)
         }
-
         saveButton.setOnClickListener {
-            val selectedStripes = mutableListOf<Int>()
-
+            val selectedStripes = mutableListOf<String>()
             for (i in 0 until stripeSpinnersContainer.childCount) {
                 val stripeSpinner = stripeSpinnersContainer.getChildAt(i) as Spinner
-                val selectedColorIndex = stripeSpinner.selectedItemPosition
-                selectedStripes.add(selectedColorIndex)
+                val selectedColor = stripeSpinner.selectedItem.toString()
+                selectedStripes.add(selectedColor)
             }
-
             val configurationName = configurationNameEditText.text.toString()
             savedConfigurations.add(Pair(configurationName, selectedStripes.toList()))
             updateConfigurationsSpinner()
             saveConfigurationsToFile()
         }
-
         deleteButton.setOnClickListener {
             val selectedConfigurationIndex = configurationsSpinner.selectedItemPosition
             if (selectedConfigurationIndex != AdapterView.INVALID_POSITION) {
@@ -151,19 +160,16 @@ class ChooseStripesActivity : AppCompatActivity() {
                 Toast.makeText(this, "Configuration deleted", Toast.LENGTH_SHORT).show()
             }
         }
-
         loadButton.setOnClickListener {
             val selectedConfigurationIndex = configurationsSpinner.selectedItemPosition
             if (selectedConfigurationIndex != AdapterView.INVALID_POSITION) {
                 val selectedStripes = savedConfigurations[selectedConfigurationIndex].second
                 val numSelectedStripes = selectedStripes.size
                 val numStripeSpinners = stripeSpinnersContainer.childCount
-
                 val numExpectedStripes = when (numStripeSpinners) {
-                    6 -> numStripeSpinners
+                    5 -> numStripeSpinners + 1 // Dodaj 1 dla paska temperaturowego
                     else -> numStripeSpinners
                 }
-
                 if (numSelectedStripes == numExpectedStripes) {
                     setStripeColors(selectedStripes)
                 } else {
@@ -171,24 +177,20 @@ class ChooseStripesActivity : AppCompatActivity() {
                 }
             }
         }
-
         loadConfigurationsFromFile()
     }
 
     private fun checkSubmitButtonState() {
         var isSubmitButtonEnabled = true
-
         for (i in 0 until stripeSpinnersContainer.childCount) {
             val stripeSpinner = stripeSpinnersContainer.getChildAt(i) as Spinner
-            val selectedColorIndex = stripeSpinner.selectedItemPosition
-            if (selectedColorIndex == 0) {
+            val selectedColor = stripeSpinner.selectedItem.toString()
+            if (selectedColor == "None") {
                 isSubmitButtonEnabled = false
                 break
             }
         }
-
         submitButton.isEnabled = isSubmitButtonEnabled
-
         if (isSubmitButtonEnabled) {
             submitButton.alpha = 1.0f
         } else {
@@ -237,20 +239,18 @@ class ChooseStripesActivity : AppCompatActivity() {
     private fun updateConfigurationsSpinner() {
         // Pobierz nazwy konfiguracji
         val configurationNames = savedConfigurations.map { it.first }
-
         // Utwórz adapter dla spinnera
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, configurationNames)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
         // Ustaw adapter dla spinnera konfiguracji
         configurationsSpinner.adapter = adapter
     }
 
-    private fun setStripeColors(colors: List<Int>) {
+    private fun setStripeColors(colors: List<String>) {
         // Ustaw kolory dla poszczególnych spinnerów
         for (i in 0 until stripeSpinnersContainer.childCount) {
             val stripeSpinner = stripeSpinnersContainer.getChildAt(i) as Spinner
-            val colorIndex = colors[i]
+            val colorIndex = getColorIndex(colors[i], i + 1)
             stripeSpinner.setSelection(colorIndex)
         }
     }
@@ -261,7 +261,7 @@ class ChooseStripesActivity : AppCompatActivity() {
             4 -> multiplierColorOptions.indexOf(color)
             5 -> toleranceColorOptions.indexOf(color)
             6 -> temperatureColorOptions.indexOf(color)
-            else -> 0
+            else -> -1
         }
     }
 
@@ -270,9 +270,7 @@ class ChooseStripesActivity : AppCompatActivity() {
         val file = File(applicationContext.filesDir, "configurations.txt")
         val fileOutputStream = FileOutputStream(file)
         val objectOutputStream = ObjectOutputStream(fileOutputStream)
-
         objectOutputStream.writeObject(savedConfigurations)
-
         objectOutputStream.close()
         fileOutputStream.close()
     }
@@ -280,22 +278,18 @@ class ChooseStripesActivity : AppCompatActivity() {
     private fun loadConfigurationsFromFile() {
         // Wczytaj konfiguracje z pliku
         val file = File(applicationContext.filesDir, "configurations.txt")
-
         if (file.exists()) {
             val fileInputStream = FileInputStream(file)
             val objectInputStream = ObjectInputStream(fileInputStream)
-
             val configurations =
-                objectInputStream.readObject() as MutableList<Pair<String, List<Int>>>
-
+                objectInputStream.readObject() as MutableList<Pair<String, List<String>>>
             // Wyczyść bieżące konfiguracje i dodaj wczytane
             savedConfigurations.clear()
             savedConfigurations.addAll(configurations)
-
             objectInputStream.close()
             fileInputStream.close()
+            // Zaktualizuj spinner konfiguracji
+            updateConfigurationsSpinner()
         }
-
-        updateConfigurationsSpinner()
     }
 }
--- app/src/main/java/pjwstk/edu/pl/resistor_calculator/PastResultsActivity.kt
+++ app/src/main/java/pjwstk/edu/pl/resistor_calculator/PastResultsActivity.kt
@@ -1,11 +1,217 @@
 package pjwstk.edu.pl.resistor_calculator
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.io.File
+import java.io.FileInputStream
+import java.io.ObjectInputStream
+import kotlin.math.pow
 
 class PastResultsActivity : AppCompatActivity() {
+    private lateinit var configurationSpinner1: Spinner
+    private lateinit var configurationSpinner2: Spinner
+    private lateinit var resultTextView1: TextView
+    private lateinit var resultTextView2: TextView
+
+    private val savedConfigurations = mutableListOf<Pair<String, List<String>>>()
+    private val resultData = mutableMapOf<Pair<String, List<String>>, String>()
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_past_results)
+
+        // Wczytaj konfiguracje z pliku
+        val configurationsLoaded = loadConfigurationsFromFile()
+        val numberOfConfigurations = savedConfigurations.size
+
+        if (configurationsLoaded && numberOfConfigurations >= 2) {
+            // Przykładowe dane - dodaj swoje zapisane wyniki
+            resultData[savedConfigurations[0]] = "Wynik 1"
+            resultData[savedConfigurations[1]] = "Wynik 2"
+
+            configurationSpinner1 = findViewById(R.id.configurationSpinner1)
+            configurationSpinner2 = findViewById(R.id.configurationSpinner2)
+            resultTextView1 = findViewById(R.id.resultTextView1)
+            resultTextView2 = findViewById(R.id.resultTextView2)
+
+            val configurationNames = savedConfigurations.map { it.first }
+            val adapter =
+                ArrayAdapter(this, android.R.layout.simple_spinner_item, configurationNames)
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+            configurationSpinner1.adapter = adapter
+            configurationSpinner2.adapter = adapter
+
+            // Ustaw domyślnie różne konfiguracje na spinnerach
+            configurationSpinner1.setSelection(0)
+            configurationSpinner2.setSelection(1)
+
+            configurationSpinner1.onItemSelectedListener =
+                object : AdapterView.OnItemSelectedListener {
+                    override fun onItemSelected(
+                        parent: AdapterView<*>?,
+                        view: View?,
+                        position: Int,
+                        id: Long
+                    ) {
+                        val selectedConfiguration = savedConfigurations[position]
+                        val result = resultData[selectedConfiguration]
+
+                        displayResult(selectedConfiguration, result ?: "", resultTextView1)
+                    }
+
+                    override fun onNothingSelected(parent: AdapterView<*>?) {
+                        // Do nothing
+                    }
+                }
+
+            configurationSpinner2.onItemSelectedListener =
+                object : AdapterView.OnItemSelectedListener {
+                    override fun onItemSelected(
+                        parent: AdapterView<*>?,
+                        view: View?,
+                        position: Int,
+                        id: Long
+                    ) {
+                        val selectedConfiguration = savedConfigurations[position]
+                        val result = resultData[selectedConfiguration]
+
+                        displayResult(selectedConfiguration, result ?: "", resultTextView2)
+                    }
+
+                    override fun onNothingSelected(parent: AdapterView<*>?) {
+                        // Do nothing
+                    }
+                }
+        } else {
+            // Brak konfiguracji lub plik nie istnieje - przejdź do ChooseStripesActivity
+            val intent = Intent(this, ChooseStripesActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun displayResult(
+        configuration: Pair<String, List<String>>,
+        result: String,
+        textView: TextView
+    ) {
+        val configurationName = configuration.first
+        val selectedStripes = configuration.second
+
+        val resistanceResult = calculateResistance(selectedStripes)
+
+        val resultText = buildString {
+            append("Config name: $configurationName\n")
+            append("Stripes colors: ${selectedStripes.joinToString(", ")}\n")
+            append("Result: $resistanceResult")
+        }
+
+        textView.text = resultText
+    }
+
+    private fun calculateResistance(selectedStripes: List<String>): String {
+        val colorsToValues = mapOf(
+            "Black" to 0,
+            "Brown" to 1,
+            "Red" to 2,
+            "Orange" to 3,
+            "Yellow" to 4,
+            "Green" to 5,
+            "Blue" to 6,
+            "Purple" to 7,
+            "Gray" to 8,
+            "White" to 9
+        )
+
+        var resistance = 0L
+        var multiplier = 1L
+
+        // Pobierz wartość rezystancji z pierwszych dwóch kolorów
+        for (i in 0..1) {
+            val color = selectedStripes[i]
+            val value = colorsToValues[color]
+            resistance = resistance * 10 + value!!
+        }
+
+        // Pobierz mnożnik z trzeciego koloru
+        val multiplierColor = selectedStripes[2]
+        val multiplierValue = colorsToValues[multiplierColor]
+        multiplier = 10.0.pow(multiplierValue!!.toDouble()).toLong()
+
+        // Oblicz wartość rezystancji
+        val resistanceValue = resistance * multiplier
+
+        // Jeśli istnieje czwarty kolor, to jest to tolerancja
+        if (selectedStripes.size > 3) {
+            val toleranceColor = selectedStripes[3]
+            val toleranceValues = mapOf(
+                "Brown" to "±1%",
+                "Red" to "±2%",
+                "Orange" to "±3%",
+                "Yellow" to "±4%",
+                "Green" to "±0.5%",
+                "Blue" to "±0.25%",
+                "Purple" to "±0.10%",
+                "Gray" to "±0.05%",
+                "Gold" to "±5%",
+                "Silver" to "±10%"
+            )
+            val toleranceValue = toleranceValues[toleranceColor]
+
+            // Jeśli istnieje piąty kolor, to jest to PPM
+            if (selectedStripes.size > 5) {
+                val ppmColor = selectedStripes[5]
+                val ppmValues = mapOf(
+                    "Black" to "250 ppm/°C",
+                    "Brown" to "100 ppm/°C",
+                    "Red" to "50 ppm/°C",
+                    "Orange" to "15 ppm/°C",
+                    "Yellow" to "25 ppm/°C",
+                    "Green" to "20 ppm/°C",
+                    "Blue" to "10 ppm/°C",
+                    "Purple" to "5 ppm/°C",
+                    "Gray" to "1 ppm/°C"
+                )
+                val ppmValue = ppmValues[ppmColor]
+                return "Resistance: $resistanceValue, Tolerance: $toleranceValue, PPM: $ppmValue"
+            } else {
+                return "Resistance: $resistanceValue, Tolerance: $toleranceValue"
+            }
+        } else {
+            return "Resistance: $resistanceValue"
+        }
+    }
+
+    private fun loadConfigurationsFromFile(): Boolean {
+        // Wczytaj konfiguracje z pliku
+        val file = File(applicationContext.filesDir, "configurations.txt")
+
+        if (file.exists()) {
+            val fileInputStream = FileInputStream(file)
+            val objectInputStream = ObjectInputStream(fileInputStream)
+
+            val configurations =
+                objectInputStream.readObject() as MutableList<Pair<String, List<String>>>
+
+            // Wyczyść bieżące konfiguracje i dodaj wczytane z pliku
+            savedConfigurations.clear()
+            savedConfigurations.addAll(configurations)
+
+            objectInputStream.close()
+            fileInputStream.close()
+
+            return true
+        } else {
+            return false
+        }
     }
 }
